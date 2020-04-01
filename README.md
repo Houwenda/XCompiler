ee7bba7cb594154e6911c6c700a67a0322668455
houwenda <houwd123@126.com>
2020-04-01 23:55:45 +0800

    fix constant production, complete production2NFA; TODO: implement NFA2DFA(),  parseCode()

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 lexical analysis
 ```shell
+tsc --build src/tsconfig.json
 cd src/build
 node index.js -l -p ~/Development/HLang/examples/t3_grammar.json -c ~/Development/HLang/examples/code.txt 
 ```
--- src/lexical_analysis.ts
+++ src/lexical_analysis.ts
@@ -1,3 +1,5 @@
+import { match } from "assert";
+
 var fs = require("fs");
 
 function findState(arrayList:Array<any>, indexStart:number, state:string ):number {
@@ -14,17 +16,16 @@ function findState(arrayList:Array<any>, indexStart:number, state:string ):numbe
 }
 
 class LexAnalyzer {
-    NFA:Array<any> = [];
-    DFA:Array<any> = [];
+    NFA:any = {};
+    DFA:any = {};
 
     production2NFA(contents:any):Array<LexNode> {
-        console.log(contents);
         var result:Array<LexNode> = [new LexNode(0, "start", "A"), new LexNode(1, "end", "END_NODE")];
 
         for (var i in contents) {
             var content:any = contents[i];
-            console.log("content: ",content);
-            
+            // console.log("content: ",content);
+
             var indexStart:number = result.length;  // nodes of this content starts at indexStart in the result array
             var description:string = content.description;
             var localStartState = content.production[0].substring(0,content.production[0].indexOf("->"));
@@ -32,17 +33,17 @@ class LexAnalyzer {
             for (var j = 0; j< content.production.length; j++) {
                 var production = content.production[j];
                 var arrowIndex:number = production.indexOf("->");
-                var left:string = production.substring(0,arrowIndex);
+                var leftState:string = production.substring(0,arrowIndex);
                 var right:string = production.substring(arrowIndex+2);
                 var rightState:any;
 
-                var matchResult:any = right.match("<.>");
+                var matchResult:any = right.match("<(.+|\n)>");
                 var char:string;
                 if (matchResult == null) {  // no char needed, e.g. "A->B", "A->C"
-                    char = "";
+                    char = "empty";
                     rightState = right;
                 } else { // char needed, e.g. "A-><digit>B", "A-><a>", "A-><empty>"
-                    char = matchResult[0].substr(1,1);
+                    char = matchResult[0].substring(1,matchResult[0].length - 1);
                     if (matchResult[0].length == right.length) {  // end of productions, e.g. "A-><a>"
                         rightState = "END_NODE";
                     } else {  // not the end, e.g. "A-><a>B"
@@ -50,7 +51,7 @@ class LexAnalyzer {
                     }
                 }
 
-                console.log(production, left, char, rightState);
+                // console.log(production, leftState, char, rightState);
 
                 /*
                 * The `left` state of a production should always appeared
@@ -60,24 +61,24 @@ class LexAnalyzer {
                 */
                 // handle right state
                 var rightStateIndex:number = findState(result, indexStart, rightState);
-                if(rightStateIndex == -1 && rightState != localStartState) {  // add new node & relation
-                    // add node
+                // add new node if needed
+                if(rightStateIndex == -1 && rightState != localStartState) {  
                     var rightStateIndex = result.length;
                     var newNode:LexNode = new LexNode(rightStateIndex, description, rightState);
                     result.push(newNode);
-                    // add relation
-                    if(left == localStartState) {  // `left` state is local start state
-                        result[0].nextState.push({
-                            "character":char,
-                            "index":rightStateIndex
-                        })
-                    } else {  // `left` state is not local start state
-
-                    }
-                    // TODO
-                } else {  // only need to add relation
-                    // TODO
+                } 
+                // add relation
+                var leftStateIndex:number;
+                if(leftState == localStartState) {  // `left` state is local start state
+                    leftStateIndex = 0;    
+                } else {  // `left` state is not local start state
+                    leftStateIndex = findState(result, indexStart, leftState);
                 }
+                result[leftStateIndex].nextState.push({
+                    "character":char,
+                    "index":rightStateIndex
+                });
+
             }
         }
         return result;
@@ -98,9 +99,10 @@ class LexAnalyzer {
         for (var i in productionsConfig) {
             var typeConfig = productionsConfig[i];
             //console.log(typeConfig);
-            if(<string>typeConfig["type"] == "operator") {
-                this.NFA[typeConfig["type"]] = this.production2NFA(typeConfig["contents"]);
-            }
+            // if(<string>typeConfig["type"] == "identifier") {
+            //     this.NFA[typeConfig["type"]] = this.production2NFA(typeConfig["contents"]);
+            // }
+            this.NFA[typeConfig["type"]] = this.production2NFA(typeConfig["contents"]);
         }
 
 
@@ -126,7 +128,18 @@ class LexAnalyzer {
     
     analyze(productionFile:string, codeFile:string):void {
         this.createNFA(productionFile);
-        console.log("NFA:",this.NFA);
+       
+        console.log("NFA:");
+        console.log(this.NFA);
+        // for (var i in this.NFA["identifier"]) {
+        //     console.log(this.NFA["identifier"][i])
+        // }
+        // console.log(this.NFA["operator"]); PASS
+        // console.log(this.NFA["keyword"]); PASS
+        // console.log(this.NFA["delimiter"]); PASS
+        // console.log(this.NFA["constant"]); PASS
+        // console.log(this.NFA["identifier"]); PASS
+
         this.NFA2DFA();
         //console.log("DFA:", this.DFA);
         this.parseCode(codeFile);
